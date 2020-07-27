7411825b6c7396ae71b8e4734a99c8fe5f2c0be7
Egill Anton Hlöðversson <egillanton@live.com>
2020-07-27 14:34:39 +0000

    Add clean up script

---
+++ s5/clean.sh
@@ -0,0 +1,30 @@
+uc_minus="\xE2\x9E\x96";
+println() { printf "$@\n" >&2; }
+
+println "Running: $0"
+println "Cleaning up current working directory"
+
+if [ -L "./utils" ]; then
+	rm  ./utils;
+	println "\t$uc_minus Symbolic link deleted: ./utils"
+fi
+
+if [ -L "./steps" ]; then
+	rm  ./steps;
+	println "\t$uc_minus Symbolic link deleted: ./steps"
+fi
+
+if [ -e "./data" ]; then
+	rm -rf ./data;
+	println "\t$uc_minus Directory deleted: ./data"
+fi
+
+if [ -e "./exp" ]; then
+	rm -rf ./exp;
+	println "\t$uc_minus Directory deleted: ./exp"
+fi
+
+if [ -e "./conf" ]; then
+	rm -rf ./conf;
+	println "\t$uc_minus Directory deleted: ./conf"
+fi
