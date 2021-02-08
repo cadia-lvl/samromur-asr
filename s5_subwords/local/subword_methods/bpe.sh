dfeb10c9207455369d54a40ca4fbe58fec4e8d2b
daviderikmollberg <davidemollberg@gamil.com>
2021-02-08 12:47:59 +0000

    Remving unnecasary comments and lines

--- s5_subwords/local/subword_methods/bpe.sh
+++ s5_subwords/local/subword_methods/bpe.sh
@@ -7,7 +7,6 @@
 lang=$1
 text_corpus=$2 
 num_merges=$3
-stage=2
 
 echo "$0: Getting PBE pairs from $text_corpus"
 python3 utils/lang/bpe/learn_bpe.py -i $text_corpus -s $num_merges > data/$lang/all/pair_codes
@@ -18,14 +17,11 @@ python3 utils/lang/bpe/apply_bpe.py -i data/$lang/all/tokens \
                                     --codes data/$lang/all/pair_codes \
                                     | sed 's/ /\n/g' | sort -u > data/$lang/all/subwords
 
-#ATh prófum þetta í stað fyrir næsta fall ef ekkert annað finnst
-#sed 's/./& /g' data/all/subwords | sed 's/@ @ //g' | sed 's/*/V/g' | paste -d ' ' data/all/subwords - > data/all/subword_lexicon
 
 python3 local/prepare_lexicon.py --i data/$lang/all/subwords \
                                 --o data/$lang/all/subword_lexicon \
                                 --is_subword True
 
-#Bætti við all, ætti ekki að laga neit held ég.
 for x in all training test; do
     utils/subword/prepare_subword_text.sh data/$lang/${x}/text \
                                         data/$lang/all/pair_codes \
