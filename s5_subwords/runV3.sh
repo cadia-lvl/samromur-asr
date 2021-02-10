d888d6866bbd64690d363ace84a41ffebd545b9b
daviderikmollberg <davidemollberg@gamil.com>
2021-02-10 15:54:54 +0000

    Killing this verison nonsense

--- s5_subwords/runV3.sh
+++ s5_subwords/runV3.sh
@@ -9,7 +9,7 @@
 #                                   David Erik Mollberg
 
 #SBATCH --mem=12G
-#SBATCH --output=logs/subset_baseline.log
+#SBATCH --output=logs/subset_bpe_does_this_work.log
 
 # set -e - Stop the script if any component returns non-zero
 # set -u - Stop the script if any variables are unbound
@@ -25,13 +25,16 @@ stage=0
 create_mfcc=true
 train_mono=true
 
-lang="test_bpe"
+lang="bpe_improvement_test"
 method='bpe'
+sw_count=1000
 
 . utils/parse_options.sh || exit 1;
 . path.sh
 . cmd.sh 
 
+#rm -r data/$lang/
+#rm -r exp/$lang
 
 #0_sb_mal_bpe_just_transcripts: subword, malrómur, Byte pair encoding, just transcripts
 #1_sb_mal_bpe_althingi: subword, malrómur, byte pair encoding,  LMtext.althingi.txt 
@@ -45,17 +48,15 @@ method='bpe'
 AUDIO=/data/asr/malromur/malromur2017/correct
 
 #samromur_root=/data/asr/samromur/samromur_ldc
-# METADATA=/data/asr/malromur/malromur2017/malromur_metadata.tsv
-METADATA=/home/derik/work/tools/normalize/malromur/normalized_files/malromur_metadata_subset.tsv
+#METADATA=/data/asr/malromur/malromur2017/malromur_metadata.tsv
+METADATA=/home/derik/work/tools/normalize/malromur/normalized_files/malromur_metadata_subset.tsv # A small subest of the corpus, used for fast testing.
 
 # Text corpus for the LM
 text_corpus=/data/asr/malromur/malromur2017/malromur_corpus.txt
 
-
 # Todo add dependency check 
 # E.g. check whether srilm is installed and the directory and two 
 # files are ok. But not necessary since instructions in readme tell the user to install it.
-
 
 if [ $stage -le 0 ]; then
 echo ============================================================================
@@ -67,36 +68,73 @@ echo ===========================================================================
 fi
 
 
-
 if [ $stage -le 1 ]; then
 echo ============================================================================
-echo "                		Using $method          "
+echo "               Create $method model and preparing text files         "
 echo ============================================================================
-  #change this to use another corpus when creating subwords
-  #path to corpus used for traning LM.
-  num_merges=1000
-  #sed "s/[^ ]* //" data/$lang/all/text > data/$lang/all/corpus
-  #text_corpus=data/$lang/all/corpus
+  # We store the models/segmentation pair codes for the different subwords methods 
+  # in data/$lang/sw. Where sw stands for subwords.
+  subword_dir=data/$lang/sw
+  mkdir -p $subword_dir
 
   if [[ $method == 'bpe' ]]; then
-    local/subword_methods/bpe.sh $lang \
-                                 $text_corpus \
-                                 $num_merges
-  fi
+    # Form a given text corpus we learn to create subwords we store the "model" as pair_codes
+    python3 local/sw_methods/bpe/learn_bpe.py -i $text_corpus \
+                                              -s $sw_count > $subword_dir/pair_codes
+    
+    # Using the pair_codes we subword tokenize the kaldi format text files
+    for x in train test eval; do
+      ./local/sw_methods/bpe/prepare_subword_text.sh data/$lang/${x}/text \
+                                                     $subword_dir/pair_codes \
+                                                     data/$lang/${x}/text 
+    done
+
+    # We again subword tokenize to create a subword lexicon
+    python3 local/sw_methods/bpe/apply_bpe.py -i data/$lang/train/tokens \
+                                              --codes $subword_dir/pair_codes \
+                                              | sed 's/ /\n/g' | sort -u > $subword_dir/subwords
+    
+  elif [[ $method == 'unigram' || $method == 'sp_bpe' ]]; then
+    echo "To be done"
+  elif [[ $method == 'morfessor' ]]; then
+    echo "To be done"
+  fi 
+  # The following scripts should be run independant of which subword method is chosen
+
+  echo "$0: Preparing lexicon"
+  python3 local/prepare_lexicon.py --i $subword_dir/subwords \
+                                   --o $subword_dir/subword_lexicon \
+                                   --is_subword True
+
+  echo "$0: Preparing lexicon, dict folder and lang folder" 
+  local/prepare_dict_subword.sh $subword_dir/subword_lexicon \
+                                data/$lang/train \
+                                data/$lang/local/dict
+
+  utils/subword/prepare_lang_subword.sh data/$lang/local/dict \
+                                        "<UNK>"\
+                                        data/$lang/local/lang \
+                                        data/$lang/lang
 fi
 
 if [ $stage -le 2 ]; then
 echo ============================================================================
-echo "                		Prepare subword text files with $method          "
+echo "                		Prepare LM with subword text files with $method          "
 echo ============================================================================
-  # This is a hack that will be replaced in future commits
-  cut -d" " -f2- data/$lang/training/text > data/$lang/training/corpus
-  text_corpus=data/$lang/training/corpus
-  
-  #local/language_modeling/prepare_lm_subword.sh data/$lang/training/text \
-  
-  
-  local/language_modeling/prepare_lm_subword.sh $text_corpus \
+
+  echo "Applying $method to $text_corpus"
+  if [[ $method == 'bpe' ]]; then
+    python3 local/sw_methods/bpe/apply_bpe.py -i $text_corpus \
+                                              --codes $subword_dir/pair_codes \
+                                              > $subword_dir/text_corpus
+
+  elif [[ $method == 'unigram' || $method == 'sp_bpe' ]]; then
+    
+  elif [[ $method == 'morfessor' ]]; then
+    echo "To be done"
+  fi 
+
+  local/lm/prepare_lm_subword.sh $subword_dir/text_corpus \
                               data/$lang/test/text \
                               data/$lang/local/dict/lexicon.txt \
                               data/$lang/local/lm \
@@ -112,18 +150,18 @@ if [ $stage -le 3 ] && $create_mfcc; then
 echo ===========================================================================
 echo "                		Creating MFCC			                "
 echo ============================================================================
-  for i in training test; do
+  for x in train test; do
     steps/make_mfcc.sh --cmd "$train_cmd" \
                        --nj $num_jobs \
-                       data/$lang/$i \
+                       data/$lang/$x \
                        exp/$lang/mfcc/log/make_mfcc \
-                       exp/$lang/mfcc/$i || error 1 "Failed creating MFCC features";
+                       exp/$lang/mfcc/$x || error 1 "Failed creating MFCC features";
 
-    steps/compute_cmvn_stats.sh data/$lang/$i \
+    steps/compute_cmvn_stats.sh data/$lang/$x \
                                 exp/$lang/mfcc/log/cmvn_stats \
-                                exp/$lang/mfcc/$i || exit 1;
+                                exp/$lang/mfcc/$x || exit 1;
     
-    utils/validate_data_dir.sh data/$lang/$i || utils/fix_data_dir.sh data/$lang/$i || exit 1;
+    utils/validate_data_dir.sh data/$lang/$x || utils/fix_data_dir.sh data/$lang/$x || exit 1;
   done
 fi
 
@@ -131,7 +169,7 @@ if [ $stage -le 4 ] && $train_mono; then
 echo ============================================================================
 echo "          Train mono system                                               "
 echo ============================================================================
-  utils/subset_data_dir.sh data/$lang/training \
+  utils/subset_data_dir.sh data/$lang/train \
                            10000 \
                            data/$lang/train.10K || exit 1;
 
@@ -149,7 +187,7 @@ if [ $stage -le 5 ]; then
   echo "$0: Aligning data using monophone system"
   steps/align_si.sh --nj $num_jobs \
                     --cmd "$train_cmd" \
-                    data/$lang/training \
+                    data/$lang/train \
                     data/$lang/lang \
                     exp/$lang/mono \
                     exp/$lang/mono_ali || exit 1;
@@ -157,7 +195,7 @@ if [ $stage -le 5 ]; then
   echo "$0: training triphone system with delta features"
   steps/train_deltas.sh --cmd "$train_cmd" \
                         2500 30000 \
-                        data/$lang/training \
+                        data/$lang/train \
                         data/$lang/lang \
                         exp/$lang/mono_ali \
                         exp/$lang/tri1 || exit 1;
@@ -169,14 +207,14 @@ if [ $stage -le 6 ]; then
   echo ============================================================================
   steps/align_si.sh --nj $num_jobs \
                     --cmd "$train_cmd" \
-                    data/$lang/training \
+                    data/$lang/train \
                     data/$lang/lang \
                     exp/$lang/tri1 \
                     exp/$lang/tri1_ali || exit 1;
 
   steps/train_lda_mllt.sh --cmd "$train_cmd" \
                           4000 50000 \
-                          data/$lang/training \
+                          data/$lang/train \
                           data/$lang/lang \
                           exp/$lang/tri1_ali \
                           exp/$lang/tri2b || exit 1;
@@ -189,21 +227,21 @@ if [ $stage -le 7 ]; then
   echo "$0: Aligning data and retraining and realigning with sat_basis"
   steps/align_si.sh --nj $num_jobs \
                     --cmd "$train_cmd" \
-                    data/$lang/training \
+                    data/$lang/train \
                     data/$lang/lang \
                     exp/$lang/tri2b \
                     exp/$lang/tri2b_ali || exit 1;
 
   steps/train_sat_basis.sh --cmd "$train_cmd" \
                            5000 100000 \
-                           data/$lang/training \
+                           data/$lang/train \
                            data/$lang/lang \
                            exp/$lang/tri2b_ali \
                            exp/$lang/tri3b || exit 1;
 
   steps/align_fmllr.sh --nj $num_jobs \
                         --cmd "$train_cmd" \
-                        data/$lang/training \
+                        data/$lang/train \
                         data/$lang/lang \
                         exp/$lang/tri3b \
                         exp/$lang/tri3b_ali || exit 1;
