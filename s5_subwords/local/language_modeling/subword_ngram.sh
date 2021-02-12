da3cbd1d7c7d005fd74c213a524108aa1d61a7c4
Inga Run <ingarunh@gmail.com>
2021-02-12 13:20:13 +0000

    fixed shellcheck errors

--- s5_subwords/local/language_modeling/subword_ngram.sh
+++ s5_subwords/local/language_modeling/subword_ngram.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 #David Erik Mollberg 2020
-#Script to call other script to create create a subword language model. 
+#Script to call other script to create create a subword language model.
 #To change the ngram-count, we need to change the prepare_lm_subword script
 
 lang=$1
@@ -7,13 +9,13 @@ corpus=$2
 ngram_count=6
 
 local/language_modeling/prepare_lm_subword.sh $corpus \
-                            data/$lang/local/dict/lexicon.txt \
-                            data/$lang/local/lm \
-                            $ngram_count \
-                            data/$lang/test/text
+data/$lang/local/dict/lexicon.txt \
+data/$lang/local/lm \
+$ngram_count \
+data/$lang/test/text
 
 utils/format_lm.sh  data/$lang/lang \
-                    data/$lang/local/lm/lm.gz \
-                    data/$lang/local/dict/lexicon.txt \
-                    data/$lang/test
+data/$lang/local/lm/lm.gz \
+data/$lang/local/dict/lexicon.txt \
+data/$lang/test
 
